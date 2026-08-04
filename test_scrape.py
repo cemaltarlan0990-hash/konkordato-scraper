c6aa868e7f97b5f1257f6d1b3478a8eb8cd02852
cemaltarlan0990-hash <cemaltarlan0990@gmail.com>
2026-08-04 11:37:05 +0300

    Update test_scrape.py

--- test_scrape.py
+++ test_scrape.py
@@ -1,4 +1,7 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://www.ilan.gov.tr/ilan/kategori/49/konkordato-ve-muhlet-iik-288inci-md-"
 
@@ -8,7 +11,7 @@ headers = {
 }
 
 try:
-    response = requests.get(url, headers=headers, timeout=15)
+    response = requests.get(url, headers=headers, timeout=15, verify=False)
     print("Status code:", response.status_code)
     print("Content length:", len(response.text))
     print("İlk 500 karakter:")
